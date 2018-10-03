7226cd5db6ac70958afb5901d8d38ad552a6b3e2
RyanBarker44 <40975009+RyanBarker44@users.noreply.github.com>
2018-10-04 01:44:53 +1000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World 2" in the terminal window.
-      System.out.println("Hello, World 2");
+      System.ot.println("Hello, World 2");
    }
 }
